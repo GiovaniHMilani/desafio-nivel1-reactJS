8508f4254656983f27f8e9cece5d3579af236849
Giovani Milani <giovanimilani11@gmail.com>
2020-04-08 14:59:00 -0300

    new: Complete challenge + README.md

---
+++ README.md
@@ -0,0 +1,41 @@
+# Desafio 02: Conceitos do Node.js
+
+Aplicação para armazenar repositórios do seu portfólio, que irá permitir a criação, listagem, atualização e remoção dos repositórios
+
+### Tecnologias Utilizadas
+
++ [React](https://pt-br.reactjs.org/) 
++ [React-DOM](https://pt-br.reactjs.org/docs/react-dom.html)
++ [React-Scripts](https://www.npmjs.com/package/react-scripts)
++ [Nodemon](https://www.npmjs.com/package/nodemon)
++ [WebPack](https://webpack.js.org/)
++ [Babel](https://babeljs.io/)
++ [Axios](https://github.com/axios/axios)
+
+### Conceitos aprendidos durante o Modulo
+
++ Como o create-react-app funciona por trás dos "panos"
++ Configurações do webpack + babel
++ Conceito de Componentização + Pratica
++ Conceito de Propriedades + Pratica
++ Conceito de Estado + Pratica
++ Requests a API node com axios
++ Utilizando async/await ou then para lidar com requisições 
+
+### Forma de utilizar
+
+```sh
+  git clone https://github.com/GiovaniHMilani/desafio-nivel1-reactJS.git
+  cd desafio-nivel1-reactJS && yarn
+```
+
+##### Rodar o Projeto
+```sh
+  yarn start
+```
+
+##### Rodar os testes
+
+```sh
+  yarn test
+```
--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repostiories, setRepositories] = useState([]);
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      title: `Curso numero ${Date.now()}`,
+      techs: ["Node.js", `${Date.now()}`],
+      url: `https://teste.teste.com/${Date.now()}`,
+    });
+    const repo = [...repostiories, response.data];
+    setRepositories(repo);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+    const repo = repostiories.filter((repository) => repository.id !== id);
+    setRepositories(repo);
   }
+
+  useEffect(() => {
+    api.get("repositories").then((response) => setRepositories(response.data));
+  }, []);
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repostiories.map((repository) => {
+          return (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          );
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
